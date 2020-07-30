6b8dadeaff8f96a98c213acb0be1f0cea3370f74
Samuel Stauffer <samuel@descolada.com>
2020-07-30 15:24:08 -0700

    Sudo install Go

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 # Install Go
 cd /usr/local && \
     rm -rf go && \
-    curl -s https://dl.google.com/go/go1.15rc1.linux-amd64.tar.gz | tar -zxf - && \
-    ln -s /usr/local/go/bin/go /usr/local/bin/go
+    curl -s https://dl.google.com/go/go1.15rc1.linux-amd64.tar.gz | sudo tar -zxf - && \
+    sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
 
