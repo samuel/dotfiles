b12dae35b7c74525023c7189cfeb4a1aad7dc58f
Samuel Stauffer <samuel@descolada.com>
2020-07-30 15:57:33 -0700

    Install golangci-lint

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+export BINDIR=$HOME/.local/bin
+
 # Install Go
 cd /usr/local && \
     rm -rf go && \
     curl -s https://dl.google.com/go/go1.15rc1.linux-amd64.tar.gz | sudo tar -zxf - && \
     sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
+
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.29.0
 
